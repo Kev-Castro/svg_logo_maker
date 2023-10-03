806bec708f4dd9d369c8feb15b54d908c64ba578
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-10-03 16:06:13 -0400

    added question prompts in index.js, also wrote code to make svg file

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
 const inquirer = require('inquirer');
 const svg = require('./lib/logo_svg');
 const { Circle, Triangle, Square } = require('./lib/shapes');
+const { writeFile } = require('fs/promises')
+
+inquirer.prompt(
+    [
+        {
+            name: 'logoText',
+            type: 'input',
+            message:
+                'Enter three character text for logo text.'
+        },
+        {
+            name: 'textColor',
+            type: 'input',
+            message:
+                'Enter text color.'
+        },
+        {
+            name: 'shapeType',
+            type: 'list',
+            message:
+                'Choose shape for your logo.',
+            choices:
+                [
+                    'Sqaure', 'Circle', 'Triangle'
+                ]
+        },
+        {
+            name: 'shapeColor',
+            type: 'input',
+            message:
+                'Enter shape color.'
+        },
+    ]
+)
+    .then(({ logoText, textColor, shapeType, shapeColor }) => {
+        let newShape;
+        switch (shapeType) {
+            case 'Square':
+                newShape = new Square();
+                break;
+
+            case 'Circle':
+                newShape = new Circle();
+                break;
+
+            default:
+                newShape = new Triangle();
+                break;
+        }
+        newShape.setColor(shapeColor);
+
+        let newSvg = new svg();
+        newSvg.setLogoText(logoText, textColor);
+        newSvg.setLogoShape(newShape);
+        writeFile('logo.svg', newSvg.render());
+    })
+    .then(() => {
+        console.log('logo.svg')
+    })
+    .catch((err) => {
+        console.log(err);
+    });
